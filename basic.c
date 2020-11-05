feeae28b502765adfab0218ed16dafebaed60c77
ulifonseca <73949910+ulifonseca@users.noreply.github.com>
2020-11-05 16:23:49 -0600

    While loop

--- basic.c
+++ basic.c
@@ -2,10 +2,17 @@
 
 int main(void)
 {
+    int b=0;
     printf("Loops execution...\n\n");
     for (int i = 0; i < 3; i++)
     {
         printf("My love miri\n");
+    }
+    printf("While loop\n");
+    while(b<5)
+    {
+        printf("%d\n",b);
+        b++;
     }
     return 0;
 }
